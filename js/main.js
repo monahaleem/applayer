05678050ea2f09a9c51369263387c39a568357ee
monahaleem <43965429+monahaleem@users.noreply.github.com>
2018-11-06 11:12:47 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,7 +4,20 @@ function myFunction() {
       x.style.display = "none";
     } else {
       x.style.display = "block";    
-      document.getElementsByClassName("icon").display="contents";
+      x.style.borderTop="1px white solid ";
+
       }
   }
-  onkeydown="keydownFunction()" onkeyup="keyupFunction()"
+  // onkeydown="keydownFunction()" onkeyup="keyupFunction()"
+  $(window).bind('scroll', function () {
+    if ($(window).scrollTop() > 200) {
+        $('nav#site-navigation').addClass('fixed');
+        $("a.marker.active:contains(Home)").addClass('item-2');        
+        $("a.marker.active:contains(SERVICES)").addClass('item-3');
+        $("a.marker.active:contains(PORTFOLIO)").addClass('item-4');
+        $("a.marker.active:contains(PRICING)").addClass('item-5');
+        $("a.marker.active:contains(TEAM)").addClass('item-6');
+    } else {
+        $('nav#site-navigation').removeClass('fixed');        
+    }
+}); 
